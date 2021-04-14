fe4415c6d8ada8190272f9e0746ee9f20548d999
yueyongyue <yueyongyue@gmail.com>
2021-04-14 10:45:18 +0800

    add main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/Kubernetes-extension/preset-pod/router"
+	"github.com/Kubernetes-extension/utils/log"
+	"github.com/Kubernetes-extension/utils/kit"
+)
+
+func main() {
+	// Debug Mode
+	gin.SetMode(gin.DebugMode)
+	g := gin.New()
+	// 设置路由
+	r := router.SetupRouter(kit.EnhanceGin(g))
+	// Listen and Server in 0.0.0.0:443
+	// tls.crt 和 tls.key 采用secret的方式挂载
+	log.Info("Listen 443")
+	if err := r.RunTLS(":443", "/etc/webhook/certs/tls.crt", "/etc/webhook/certs/tls.key"); err != nil {
+		log.Fatalf("Listen error: %v", err)
+	}
+}
